c3d3d9927a0446d3cee68d6d1f79acd1eb38636c
Arthur Hamon <arthur@scorenco.com>
2015-07-27 15:38:01 +0200

    Add setup.py to get it from pip.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='pushbots',
+    version='1.0.0',
+    author='scorenco',
+    author_email='noreply@scorenco.com',
+    packages=['pushbots', ],
+    scripts=[],
+    url='http://github.com/arthurHamon2/pushbots/',
+    license='LICENSE.txt',
+    description='Pushbots REST client library',
+    long_description=open('README.md').read(),
+    install_requires=[
+    ],
+)
